4e4388177e202d3594532377f7e55429bae975ef
alancarrillo <alang.carrillo@alumnos.ugd.mx>
2022-12-02 00:43:21 -0600

    softdelete

--- app/Models/Contacto.php
+++ app/Models/Contacto.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Contacto extends Model
 {
-    use HasFactory; 
+    use HasFactory;
+    use SoftDeletes; 
     
     protected $fillable = ['nombre', 'correo', 'comentario'];
     //protected $guarded = ['id', '_token'];
--- app/Models/cliente.php
+++ app/Models/cliente.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class cliente extends Model
 {
+    use SoftDeletes;
     use HasFactory;
     protected $fillable = ['nombre', 'correo', 'telefono', 'direccion'];
     public $timestamps = false;
